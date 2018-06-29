0fd74d8074cf58c72f42d2bfc19c1b84b2ea5585
jigkoh3 <jigkoh3@hotmail.com>
2018-06-29 12:37:59 +0700

    [edit] face detect

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,6 +14,7 @@ declare var tracking: any;
 })
 export class MyApp {
   @ViewChild('canvas') canvas: ElementRef;
+  @ViewChild('video') video: ElementRef;
   rootPage: any = HomePage;
   react: any = 'dook';
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
@@ -21,16 +22,17 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
       let canvas: any = this.canvas.nativeElement;
+      let video: any = this.video.nativeElement;
       let context: any = canvas.getContext('2d');
       context.strokeStyle = '#00e000';
-          //context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-          context.strokeRect(100, 10, 50, 50);
+      //context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+      context.strokeRect(100, 10, 50, 50);
 
 
-      //let tracker = new tracking.ObjectTracker('face');
-      let tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
-      tracker.setInitialScale(4);
-      tracker.setStepSize(0.2);
+      let tracker = new tracking.ObjectTracker('face');
+      //let tracker = new tracking.ObjectTracker(['face', 'eye', 'mouth']);
+      tracker.setInitialScale(6);
+      tracker.setStepSize(2);
       tracker.setEdgesDensity(0.1);
       let task = tracking.track('#video', tracker, { camera: true });
       tracker.on('track', function (event) {
@@ -48,12 +50,34 @@ export class MyApp {
         //   context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
         //   context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
         // });
+        context.clearRect(0, 0, canvas.width, canvas.height);
         if (event.data.length === 0) {
           // No colors were detected in this frame.
         } else {
-          event.data.forEach(function(rect) {
+          event.data.forEach(function (rect) {
+
+            // if (navigator.mediaDevices) {
+            //   // access the web cam
+            //   navigator.mediaDevices.getUserMedia({video: true})
+            //   // permission granted:
+            //     .then(function(stream) {
+            //       video.src = window.URL.createObjectURL(stream);
+            //       console.log(stream);
+            //     })
+            //     // permission denied:
+            //     .catch(function(error) {
+            //       document.body.textContent = 'Could not access the camera. Error: ' + error.name;
+            //     });
+            // }
+
             // rect.x, rect.y, rect.height, rect.width, rect.color
-            console.log(rect.x, rect.y, rect.height, rect.width, rect.color);
+            console.log(rect.x, rect.y, rect.height, rect.width, event);
+            // context.strokeStyle = '#00e000';
+            // context.clearRect(0, 0, canvas.width, canvas.height);
+            // context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+            //context.strokeRect(rect.x, 10, rect.width, rect.height);
+            context.strokeStyle = '#00e000';
+            context.strokeRect(rect.x, rect.y, 100, 100);
           });
         }
       });
