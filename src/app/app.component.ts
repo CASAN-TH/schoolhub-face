82391e498394f92a3035535569c5c125903e63a6
kanomdook <kanung.2535@gmail.com>
2018-06-29 11:45:40 +0700

    [edit] getUserMedia

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -5,6 +5,7 @@ import { SplashScreen } from '@ionic-native/splash-screen';
 import { HomePage } from '../pages/home/home';
 import 'tracking/build/tracking';
 import 'tracking/build/data/face';
+import { SELECT_VALUE_ACCESSOR } from '@angular/forms/src/directives/select_control_value_accessor';
 declare var tracking: any;
 
 @Component({
@@ -12,6 +13,7 @@ declare var tracking: any;
 })
 export class MyApp {
   @ViewChild('canvas') canvas: ElementRef;
+  @ViewChild('video') video: ElementRef;
   rootPage: any = HomePage;
   react: any = 'dook';
   constructor(platform: Platform, statusBar: StatusBar, splashScreen: SplashScreen) {
@@ -19,10 +21,10 @@ export class MyApp {
       statusBar.styleDefault();
       splashScreen.hide();
       let canvas: any = this.canvas.nativeElement;
+      let video: any = this.video.nativeElement;
       let context: any = canvas.getContext('2d');
       context.strokeStyle = '#00e000';
-          //context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-          context.strokeRect(100, 10, 50, 50);
+      context.strokeRect(100, 10, 50, 50);
 
 
       let tracker = new tracking.ObjectTracker('face');
@@ -31,19 +33,21 @@ export class MyApp {
       tracker.setEdgesDensity(0.1);
       let task = tracking.track('#video', tracker, { camera: true });
       tracker.on('track', function (event) {
-        //alert(JSON.stringify(event));
-        console.log('event ready');
-        //context.clearRect(0, 0, canvas.width, canvas.height);
         event.data.forEach(function (rect) {
-          console.log(rect);
-          context.strokeStyle = '#00e000';
-           context.clearRect(0, 0, canvas.width, canvas.height);
-          //context.strokeRect(rect.x, rect.y, rect.width, rect.height);
-          context.strokeRect(rect.x, 10, rect.width, rect.height);
-          context.font = '11px Helvetica';
-          context.fillStyle = "#00e000";
-          context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
-          context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
+          navigator.getUserMedia({ video: true }, function (stream) {
+            let blob: any = window.URL.createObjectURL(stream);
+            console.log(blob);
+
+          }, function (error) { console.error(error) });
+          // console.log(rect);
+          // context.strokeStyle = '#00e000';
+          // context.clearRect(0, 0, canvas.width, canvas.height);
+          // //context.strokeRect(rect.x, rect.y, rect.width, rect.height);
+          // context.strokeRect(rect.x, 10, rect.width, rect.height);
+          // context.font = '11px Helvetica';
+          // context.fillStyle = "#00e000";
+          // context.fillText('x: ' + rect.x + 'px', rect.x + rect.width + 5, rect.y + 11);
+          // context.fillText('y: ' + rect.y + 'px', rect.x + rect.width + 5, rect.y + 22);
         });
       });
     });
