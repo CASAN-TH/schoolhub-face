23fbcc5258922b86d82b69026b437703ee88eab1
jigkoh3 <jigkoh3@hotmail.com>
2018-07-05 10:29:21 +0700

    xzc

--- src/pages/home/home.ts
+++ src/pages/home/home.ts
@@ -33,59 +33,38 @@ export class HomePage {
   }
 
   ionViewDidLoad() {
+    this.faceDetecting();
+    this.train();
+    //this.theLoop();
+  }
 
+  faceDetecting(){
     const tracker = new tracking.ObjectTracker('face');
     tracker.setInitialScale(4);
     tracker.setStepSize(0.5);
     tracker.setEdgesDensity(0);
     const trackingTask = tracking.track('#video', tracker, { camera: true });
     trackingTask.run();
+    trackingTask.stop();
     // on tracker start, if we found face (event.data)
-    tracker.on('track', function (event) {
-      //console.log(event);
-      if (event.data.length > 0) {
-        var _video: any = document.querySelector('video');
-        var _canvas: any = document.createElement('canvas');
-        _canvas.height = _video.videoHeight;
-        _canvas.width = _video.videoWidth;
-        var ctx = _canvas.getContext('2d');
-        ctx.drawImage(_video, 0, 0, _canvas.width, _canvas.height);
-        var img = new Image();
-        img.src = _canvas.toDataURL();
-        window.localStorage.setItem('face', img.src);
-      }
-      setTimeout(() => {
-        trackingTask.stop();
-      }, 5500);
-    });
-    // this.task = tracking.track('#video', this.tracker, { camera: true });
-    // this.task.run();
-    // this.tracker.on('track', event => {
+    // tracker.on('track', function (event) {
+      
+    //   //console.log(event);
+    //   // if (event.data.length > 0) {
+    //   //   var _video: any = document.querySelector('video');
+    //   //   var _canvas: any = document.createElement('canvas');
+    //   //   _canvas.height = _video.videoHeight;
+    //   //   _canvas.width = _video.videoWidth;
+    //   //   var ctx = _canvas.getContext('2d');
+    //   //   ctx.drawImage(_video, 0, 0, _canvas.width, _canvas.height);
+    //   //   var img = new Image();
+    //   //   img.src = _canvas.toDataURL();
+    //   //   window.localStorage.setItem('face', img.src);
+    //   // }
     //   setTimeout(() => {
-    //     this.task.stop();
+    //     trackingTask.stop();
     //   }, 5500);
-    //   if (event.data.length === 0) {
-
-    //   } else {
-    //     var _video: any = document.querySelector('video');
-    //     var _canvas: any = document.createElement('canvas');
-    //     _canvas.height = _video.videoHeight;
-    //     _canvas.width = _video.videoWidth;
-    //     var ctx = _canvas.getContext('2d');
-    //     event.data.forEach(rect => {
-    //       ctx.drawImage(_video, 0, 0, _canvas.width, _canvas.height);
-    //       var img = new Image();
-    //       img.src = _canvas.toDataURL();
-    //       window.localStorage.setItem('face', img.src);
-    //      console.log('object');
-    //     });
-
-    //   }
-    //   this.task.stop();
-    // })
-
-    this.train();
-    this.theLoop();
+    // });
   }
 
   theLoop() {
