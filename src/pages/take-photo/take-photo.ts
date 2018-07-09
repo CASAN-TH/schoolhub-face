047a2bec685286c0218a78ebb4dbda49c7bafa88
kanomdook <kanung.2535@gmail.com>
2018-07-09 18:00:24 +0700

    [success]

--- src/pages/take-photo/take-photo.ts
+++ src/pages/take-photo/take-photo.ts
@@ -1,13 +1,9 @@
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ModalController } from 'ionic-angular';
 import { CameraPreview } from '@ionic-native/camera-preview';
-
-/**
- * Generated class for the TakePhotoPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
+import { FaceServiceProvider } from '../../providers/face-service/face-service';
+import { CreatePersonModalPage } from '../create-person-modal/create-person-modal';
+import { AuthServiceProvider } from '../../providers/auth-service/auth-service';
 
 @IonicPage()
 @Component({
@@ -15,12 +11,27 @@ import { CameraPreview } from '@ionic-native/camera-preview';
   templateUrl: 'take-photo.html',
 })
 export class TakePhotoPage {
+  faces: Array<any> = [];
+  person: any;
+  constructor(public auth: AuthServiceProvider, public modalCtrl: ModalController, public faceServiceProvider: FaceServiceProvider, public cameraPreview: CameraPreview, public navCtrl: NavController, public navParams: NavParams) {
 
-  constructor(public cameraPreview: CameraPreview, public navCtrl: NavController, public navParams: NavParams) {
   }
 
   ionViewDidLoad() {
-
+    let modal = this.modalCtrl.create(CreatePersonModalPage);
+    modal.onDidDismiss(res => {
+      if (res) {
+        this.faceServiceProvider.CreatePerson(this.auth.Uesr().schoolid, res).then(res => {
+          //alert(JSON.stringify(res));
+          this.person = res;
+        }).catch(err => {
+          alert(JSON.stringify(err));
+        });
+      } else {
+        this.navCtrl.pop();
+      }
+    });
+    modal.present();
   }
 
   cameraSwitch() {
@@ -32,6 +43,27 @@ export class TakePhotoPage {
     this.cameraPreview.takePicture().then((imageData) => {
       let img = 'data:image/jpeg;base64,' + imageData;
       //this.detect(img);
+      this.faceServiceProvider.DetectStream(img).then(res => {
+        let data: any = res;
+        //alert(JSON.stringify(data));
+        if (data.length > 0) {
+          this.faceServiceProvider.AddPersonFaceStream(this.auth.Uesr().schoolid, this.person.personId, img).then(res => {
+            //alert(JSON.stringify(res));
+            this.faces.push(res);
+            if (this.faces.length >= 3) {
+              this.faceServiceProvider.TrainPersonGroup(this.auth.Uesr().schoolid).then(res => {
+                this.navCtrl.pop();
+              }).catch(err => {
+                alert(JSON.stringify(err));
+              });
+            }
+          }).catch(err => {
+            alert(JSON.stringify(err));
+          });
+        }
+      }).catch(err => {
+        alert(JSON.stringify(err));
+      });
     }, (err) => {
       console.log(err);
     });
