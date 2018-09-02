e5b37f8a5583e03c279ec841f671e74fecfddf46
kanomdook <kanung.2535@gmail.com>
2018-09-02 15:58:20 +0700

    [edit] screen size

--- src/app/app.constant.ts
+++ src/app/app.constant.ts
@@ -1,4 +1,4 @@
 export class Constants {
     //public static get URL(): string { return "https://school-hub-api-dev.herokuapp.com"; };  // DEV
-    public static get URL(): string { return "https://school-hub-api.herokuapp.com"; };  // DEV
+    public static get URL(): string { return "https://school-hub-api.herokuapp.com"; };  // PROD
 }
